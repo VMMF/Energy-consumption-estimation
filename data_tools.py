547473672e5023dab56a950f87f45b4b919453bf
Victor Mendez <vmendezfonseca@gmail.com>
2021-05-27 07:55:17 -0300

    Starting on Data De-trend by differentiation

--- data_tools.py
+++ data_tools.py
@@ -44,3 +44,53 @@ def rmse(y_true, y_pred):
 
 
 #TODO check if timeseries is stationary, otherwise detrend    
+
+def timeseries_diff (timeseries,interval=1):
+    """
+    A method to remove a possible linear trend in the data by differentiation. 
+    Can be called repeatedly for quadratic, cubic trend, etc.
+    """
+
+    diff = list()
+    for i in range(interval, len(timeseries)):
+        value = timeseries[i] - timeseries[i - interval]
+        diff.append(value)
+    return diff
+
+# def timeseries_inv_diff (timeseries_diff):
+#     inverted = list()
+#     for i in range(len(timeseries_diff)):
+#         value = yhat[i] + history[-interval]
+#         # value = inverse_difference(series, differenced[i], len(series)-i)
+#         inverted.append(value)
+
+
+
+
+# from pandas import read_csv
+# from pandas import datetime
+# from sklearn.linear_model import LinearRegression
+# from matplotlib import pyplot
+# import numpy
+ 
+# def parser(x):
+# 	return datetime.strptime('190'+x, '%Y-%m')
+ 
+# series = read_csv('shampoo-sales.csv', header=0, parse_dates=[0], index_col=0, squeeze=True, date_parser=parser)
+# # fit linear model
+# X = [i for i in range(0, len(series))]
+# X = numpy.reshape(X, (len(X), 1))
+# y = series.values
+# model = LinearRegression()
+# model.fit(X, y)
+# # calculate trend
+# trend = model.predict(X)
+# # plot trend
+# pyplot.plot(y)
+# pyplot.plot(trend)
+# pyplot.show()
+# # detrend
+# detrended = [y[i]-trend[i] for i in range(0, len(series))]
+# # plot detrended
+# pyplot.plot(detrended)
+# pyplot.show()
